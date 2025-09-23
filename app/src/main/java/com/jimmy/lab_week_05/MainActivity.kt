93c3555ad6ebbfdbf8be177faf4dad49237872bb
Jimagination <jimmytandra65@gmail.com>
2025-09-23 15:34:56 +0700

    Assignment

--- app/src/main/java/com/jimmy/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/jimmy/lab_week_05/MainActivity.kt
@@ -59,13 +59,16 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
                 if (response.isSuccessful) {
                     val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl.orEmpty()
-                    if(firstImage.isNotBlank()) {
-                        imageLoader.loadImage(firstImage, imageResultView)
+                    val firstImage = image?.firstOrNull()
+
+                    val imageUrl = firstImage?.imageUrl.orEmpty()
+                    if(imageUrl.isNotBlank()) {
+                        imageLoader.loadImage(imageUrl, imageResultView)
                     } else {
                         Log.d(MAIN_ACTIVITY, "Missing image URL")
                     }
-                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
+                    val breedName = firstImage?.breeds?.firstOrNull()?.name ?: "Unknown"
+                    apiResponseView.text = getString(R.string.breed_info, breedName)
                 } else {
                     Log.e(MAIN_ACTIVITY, "Failed to get response\n" + response.errorBody()?.string().orEmpty())
                 }
